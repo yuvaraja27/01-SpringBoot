bb9d1f6d24c73c5c164e59d09fc2c2be7fe50831
Yuva Raja Manohar Kola <yuvarajakola@gmail.com>
2024-01-28 21:39:03 +0530

    Project Initial Commit

---
+++ src/main/java/com/in/ashokit/database/SprinBootGitHubApplication.java
@@ -0,0 +1,13 @@
+package com.in.ashokit.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinBootGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinBootGitHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/in/ashokit/database/SprinBootGitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in.ashokit.database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinBootGitHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
